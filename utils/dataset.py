04334c32eadb62afddc5cdc4857973e004b12af1
Houssam Zenati <houssam.zenati@inria.fr>
2022-12-07 14:16:30 +0100

    warfarin, personalized-pricing

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,17 +5,17 @@ from abc import ABCMeta, abstractmethod
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression, RidgeCV
+
 
 dataset_dico = {
-    'open': 'OpenDataset',
-    'noisycircles': 'Synthetic',
-    'noisymoons': 'Synthetic',
-    'anisotropic': 'Synthetic',
-    'gmm': 'Synthetic',
-    'varied': 'Synthetic',
-    'toy-gmm': 'Synthetic',
+    'noisycircles': 'PotentialPrediction',
+    'noisymoons': 'PotentialPrediction',
+    'anisotropic': 'PotentialPrediction',
     'warfarin': 'WarfarinDataset',
+    'personalized-pricing': 'PersonalizedPricing'
 }
+from scipy.stats import norm
 
 def get_dataset_by_name(name, random_seed=42):
     mod = __import__("utils.dataset", fromlist=[dataset_dico[name]])
@@ -56,7 +56,7 @@ class Dataset:
         """
         return np.exp(-(np.log(action) - mu) ** 2 / (2 * sigma ** 2)) / (action * sigma * np.sqrt(2 * np.pi))
 
-class Synthetic(Dataset):
+class PotentialPrediction(Dataset):
     """Parent class for Data
 
     """
@@ -77,7 +77,7 @@ class Synthetic(Dataset):
         Note:
             Setup done in auxiliary private method
         """
-        super(Synthetic, self).__init__(**kw)
+        super(PotentialPrediction, self).__init__(**kw)
         self.name = name
         self.dimension = 2
         # self.n_samples = n_samples
@@ -105,26 +105,23 @@ class Synthetic(Dataset):
         return np.abs(potentials)
 
     def get_X_y(self, n_samples):
+
         if self.name == 'noisycircles':
             return datasets.make_circles(n_samples=n_samples, factor=.5,
                                          noise=.05, random_state=self.random_seed)
         elif self.name == 'noisymoons':
             return datasets.make_moons(n_samples=n_samples, noise=.05, random_state=self.random_seed)
-        elif self.name == 'gmm':
-            return datasets.make_blobs(n_samples=n_samples, random_state=self.random_seed)
+
         elif self.name == 'anisotropic':
             X, y = datasets.make_blobs(n_samples=n_samples, centers=3,
                                        cluster_std=[[1 / 2, 1], [3 / 2, 1 / 2], [1, 3 / 2]],
                                        random_state=self.random_seed)
             X = np.dot(X, self.rng.randn(2, 2))
             return X, y
-        elif self.name == 'toy-gmm':
-            X, y = datasets.make_blobs(centers=2, cluster_std=[[3, 1], [3, 2]],
-                                       n_samples=n_samples, random_state=self.random_seed)
-            return X, y
+
         else:
-            return datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5],
-                                       random_state=self.random_seed)
+            return
+
 
     def sample_logged_data(self, n_samples):
         """ Setup the experiments and creates the data
@@ -134,16 +131,99 @@ class Synthetic(Dataset):
         actions = self.rng.lognormal(mean=self.start_mu, sigma=self.start_sigma, size=n_samples)
         losses = self.get_losses_from_actions(potentials, actions)
         propensities = self.logging_policy(actions, self.start_mu, self.start_sigma)
-        return actions, features, losses, propensities, potentials, labels
+        return actions, features, losses, propensities, potentials
 
     def sample_data(self, n_samples):
-        _, contexts, _, _, potentials, _ = self.sample_logged_data(n_samples)
+        _, contexts, _, _, potentials = self.sample_logged_data(n_samples)
         return contexts, potentials
 
     @staticmethod
     def get_losses_from_actions(potentials, actions):
         return - np.maximum(np.where(actions < potentials, actions / potentials, -0.5 * actions + 1 + 0.5 * potentials),
                           -0.1)
+
+
+class PersonalizedPricing(Dataset):
+    """Parent class for Data
+
+    """
+
+    def __init__(self, name, mode='quadratic', **kw):
+        """Initializes the class
+
+        Attributes:
+            name (str): name of the dataset
+            n_samples (int): number of samples
+            start_mean (float): starting mean of the logging policy
+            start_std (float): starting std of the logging policy
+            start_sigma (float): starting parameter sigma of the logging policy
+            start_mu (float): starting parameter mu of the logging policy
+            mus (list): list of means of the potential group labels
+            potentials_sigma (float): variance of the potential group labels
+
+        Note:
+            Setup done in auxiliary private method
+        """
+        super(PersonalizedPricing, self).__init__(**kw)
+        self.name = name
+        self.dimension = 10
+        self.l = 3
+        # self.n_samples = n_samples
+        self.start_mean = 2.
+        self.mode = mode
+        self.a, self.b = self.get_functions(self.mode)
+        self.test_data = self.sample_data(10000)
+
+    def get_functions(self, mode):
+        if mode == 'quadratic':
+            a = lambda z: 2 * z ** 2
+            b = lambda z: 0.6 * z
+        elif mode == 'step':
+            a = lambda z: np.where((z < 1.5), 5, 6)
+            b = lambda z: np.where((z < 1.5), 0.7, 1.2)
+        elif mode == 'sigmoid':
+            a = lambda z: 1 / (1 + np.exp(z))
+            b = lambda z: 2 / (1 + np.exp(z)) + 0.1
+        else:
+            a = lambda z: 6 * z
+            b = lambda z: z
+        return a, b
+
+
+    def sample_logged_data(self, n_samples):
+        """ Setup the experiments and creates the data
+        """
+        z = self.rng.uniform(low=1, high=2, size=(n_samples, self.dimension))
+        z_bar = self._get_potentials(z)
+        p = self.rng.normal(loc=z_bar, scale=1)
+        losses = self.get_losses_from_actions(z_bar, p)
+        propensities = norm(loc=z_bar, scale=1).pdf(p)
+
+        return p, z, losses, propensities, z_bar
+
+    def sample_data(self, n_samples):
+        _, contexts, _, _, potentials = self.sample_logged_data(n_samples)
+        return contexts, potentials
+
+    def _get_potentials(self, z):
+        """
+        Args
+            z (np.array): group labels
+
+        """
+        return np.mean(z[:, :self.l], axis=1)
+
+    def get_losses_from_actions(self, z_bar, actions):
+        epsilon_noise = self.rng.normal(loc=np.zeros_like(z_bar), scale=1)
+        return - (actions * (self.a(z_bar) - self.b(z_bar) * actions) + epsilon_noise)
+
+    def get_optimal_parameter(self):
+        z, z_bar = self.sample_data(10000)
+        pistar_determinist = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1])
+        optimal_prices = self.a(z_bar) / (2 * self.b(z_bar))
+        pistar_determinist.fit(z, optimal_prices)
+        return np.concatenate([np.array([pistar_determinist.intercept_]), pistar_determinist.coef_])
+
 
 class WarfarinDataset(Dataset):
     """ Warfarin Data
@@ -165,55 +245,86 @@ class WarfarinDataset(Dataset):
         self.path = path
         self.file_name = "warfarin.npy"
         self.name = name
-        self.features_length = 81
-        self._load_and_setup_data()
+        self.dimension = 81
         self.train_size = 2 / 3
         self.val_size = 0.5
+        self._load_and_setup_data()
+
 
     def _load_and_setup_data(self):
         """ Load data from csv file
         """
         file_path = os.path.join(self.path, self.file_name)
         data = np.load(file_path)
-        features = data[:, :self.features_length]
-        actions = data[:, self.features_length]
-        losses = - data[:, self.features_length+1]
-        pi_logging = data[:, self.features_length+2]
-        potentials = data[:, self.features_length+3]
+        features = data[:, :self.dimension]
+        actions = data[:, self.dimension]
+        losses = - data[:, self.dimension+1]
+        propensities = data[:, self.dimension+2]
+        potentials = data[:, self.dimension+3]
 
         self.mu_dose = np.std(potentials)
 
         idx = self.rng.permutation(features.shape[0])
-        features, actions, rewards, pi_logging, potentials = features[idx], actions[idx], losses[idx], \
-                                                             pi_logging[idx], potentials[idx]
+        features, actions, losses, propensities, potentials = features[idx], actions[idx], losses[idx], \
+                                                             propensities[idx], potentials[idx]
 
         size = int(features.shape[0] * self.train_size)
         a_train, self.actions_test = actions[:size], actions[size:]
         f_train, self.features_test = features[:size, :], features[size:, :]
-        r_train, self.reward_test = rewards[:size], rewards[size:]
-        pi_0_train, self.pi_0_test = pi_logging[:size], pi_logging[size:]
+        l_train, self.losses_test = losses[:size], losses[size:]
+        propensities_train, self.propensities_test = propensities[:size], propensities[size:]
         potentials_train, self.potentials_test = potentials[:size], potentials[size:]
 
         size = int(features.shape[0] * self.val_size)
         self.actions_train, self.actions_valid = a_train[:size], a_train[size:]
         self.features_train, self.features_valid = f_train[:size, :], f_train[size:, :]
-        self.reward_train, self.reward_valid = r_train[:size], r_train[size:]
-        self.pi_0_train, self.pi_0_valid = pi_0_train[:size], pi_0_train[size:]
+        self.losses_train, self.losses_valid = l_train[:size], l_train[size:]
+        self.propensities_train, self.propensities_valid = propensities_train[:size], propensities_train[size:]
         self.potentials_train, self.potentials_valid = potentials_train[:size], potentials_train[size:]
 
-        self.baseline_reward_valid = np.mean(self.reward_valid)
-        self.baseline_reward_test = np.mean(self.reward_test)
+        self.baseline_loss_valid = np.mean(self.losses_valid)
+        self.baseline_loss_test = np.mean(self.losses_test)
 
-    def get_potentials_labels(self, mode):
-        if mode=='train':
-            return self.potentials_train, np.empty_like(self.potentials_train)
-        elif mode=='test':
-            return self.potentials_test, np.empty_like(self.potentials_test)
-        else:
-            return self.potentials_valid, np.empty_like(self.potentials_valid)
+
+        self.test_data = self.features_test, self.potentials_test
+
+    def get_logged_data(self):
+        """ Setup the experiments and creates the data
+        """
+        return self.actions_train, self.features_train, self.losses_train, self.propensities_train, self.potentials_train
 
     def get_losses_from_actions(self, potentials, actions):
         return np.maximum(np.abs(potentials - actions) - 0.1*potentials, 0.)
+
+    def sample_logged_data(self, n_samples):
+        """ Setup the experiments and creates the data
+        """
+        return next(self.data_generator)
+
+    def sample_data(self, n_samples):
+        _, contexts, _, _, potentials = self.sample_logged_data(n_samples)
+        return contexts, potentials
+
+    def set_data_generator(self, n_samples):
+        self.data_generator = self._infinite_generator(n_samples)
+
+    # Convert data into a stream of never-ending data
+    def _infinite_generator(self, batch_size):
+
+        actions, features, losses, propensities, potentials = self.actions_train, self.features_train, \
+                                                              self.losses_train, self.propensities_train, \
+                                                              self.potentials_train
+        i = 0  # Type: int
+        while True:
+            j = i + batch_size
+            # If we wrap around the back of the dataset:
+            if j >= actions.shape[0]:
+                rv = list(range(i, actions.shape[0])) + list(range(0, j - actions.shape[0]))
+                yield (actions[rv], features[rv, ...], losses[rv], propensities[rv], potentials[rv])
+                i = j - actions.shape[0]
+            else:
+                yield (actions[i:j], features[i:j, ...], losses[i:j], propensities[i:j], potentials[i:j])
+                i = j
 
 
 

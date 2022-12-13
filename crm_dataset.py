fixed variance penalty computation

crm_dataset.py
@@ -2,15 +2,16 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 
+
 class CRMDataset(object):
-    
     def __init__(self):
 
         self.propensities_ = []
         self.actions_ = []
         self.rewards_ = []
         self.features_ = []
-        
+        self.rollouts_ = []
+
         self.propensities_np = None
         self.actions_np = None
         self.rewards_np = None
@@ -67,7 +68,6 @@ class CRMDataset(object):
         # y is (n,k)
         # probas is (n,k)
         n = X.shape[0]
-        d = X.shape[1]
         k = y.shape[1]
 
         assert len(X) == len(y) == len(probas), (len(X) , len(y) , len(probas))
@@ -83,7 +83,7 @@ class CRMDataset(object):
             self.propensities_ += [propensities]
             self.features_ += [np.array(X)]
             rewards = (1 - np.logical_xor(actions, y)).sum(axis=1).reshape((n,1))
-            assert rewards.shape == (n,1), rewards.shape
+            assert rewards.shape == (n, 1), rewards.shape
             self.rewards_ += [rewards]
                 
         self._generate_arrays()
@@ -91,9 +91,25 @@ class CRMDataset(object):
         assert self.features.shape[0] == self.actions.shape[0] == self.propensities.shape[0] == self.rewards.shape[0]
         assert self.actions.shape[1] == self.propensities.shape[1]
         assert self.rewards.shape[1] == 1
+
+        self.rollouts_ += [n * n_samples]
+
+        self.check()
         
         return self
-    
+
+    @property
+    def rollout_indices(self):
+        return np.array(list(self.iter_rollout_indices()))
+
+    def iter_rollout_indices(self):
+        start = 0
+        end = 0
+        for rollout_length in self.rollouts_:
+            end += rollout_length
+            yield start, end
+            start = end
+
     def split(self, ratio=.25, seed=0, shuffle=True):
         p_1, p_2, a_1, a_2, f_1, f_2, r_1, r_2 = train_test_split(
             self.propensities, self.actions, self.features, self.rewards,
@@ -115,6 +131,7 @@ class CRMDataset(object):
         d2._generate_arrays()
         
         return d1, d2
-    
+
+
 if __name__ == '__main__':
     CRMDataset()

crm_model.py
@@ -2,6 +2,9 @@ import sys
 import numpy as np
 
 import jax
+
+from crm_dataset import CRMDataset
+
 jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import jaxopt
@@ -56,8 +59,7 @@ class Model(object):
     def expected_hamming_loss(self, X, y):
         y_invert = 1 - y
         invert_probas = self.predict_proba(X, y_invert)
-        return invert_probas.sum() / (self.k * y.shape[0])       
-    
+        return invert_probas.sum() / (self.k * y.shape[0])
     
     def check_propensity_overfitting(self, ips_weights):
         std = jnp.std(ips_weights)
@@ -70,8 +72,22 @@ class Model(object):
 #             jax.debug.print("WARN: propensity overfitting detected")
         return (avg < 2).astype(int)
 
+    def variance_penalty(self, rollout_indices, per_instance_importance_weighted_rewards):
+        penalty = 0
+        for start1, end1 in rollout_indices:
+            for start2, end2 in rollout_indices:
+                if start1 < start2:
+                    continue
+                rollout1_data = per_instance_importance_weighted_rewards[start1:end1]
+                rollout2_data = per_instance_importance_weighted_rewards[start2:end2]
+                rollout1_data = rollout1_data[:len(rollout1_data)][:len(rollout2_data)]
+                rollout2_data = rollout2_data[:len(rollout1_data)][:len(rollout2_data)]
+                cov = jnp.multiply(rollout1_data, rollout2_data).mean() - rollout1_data.mean()*rollout2_data.mean()
+                penalty += cov
+        penalty = jnp.sqrt(penalty / len(per_instance_importance_weighted_rewards))
+        return penalty
 
-    def crm_loss(self, crm_dataset, 
+    def crm_loss(self, crm_dataset: CRMDataset,
                  snips = True,
                  lambda_: float = 0,
                  max_per_instance_ips = 5e4,
@@ -131,8 +147,8 @@ class Model(object):
         per_instance_importance_weights = jnp.clip(per_instance_importance_weights, min_per_instance_importance_weights, max_per_instance_ips)        
 
         if verbose > 1: jax.debug.print('\tclipped IPS: {} - {}', 
-                                    per_instance_importance_weights.min(), 
-                                    per_instance_importance_weights.max())
+                                        per_instance_importance_weights.min(),
+                                        per_instance_importance_weights.max())
         
         per_instance_importance_weights = per_instance_importance_weights.reshape(
             (per_instance_importance_weights.shape[0], 1)
@@ -144,8 +160,8 @@ class Model(object):
             per_instance_importance_weights
         )
         if verbose > 1: jax.debug.print('\tIPS-R: {} - {}', 
-                                    per_instance_importance_weighted_rewards.min(), 
-                                    per_instance_importance_weighted_rewards.max())
+                                        per_instance_importance_weighted_rewards.min(),
+                                        per_instance_importance_weighted_rewards.max())
         
         # ESS
         if compute_ess:
@@ -161,14 +177,14 @@ class Model(object):
         
         # POEM
         if lambda_ != 0:
-            per_instance_importance_weighted_rewards_variance = jnp.sum(jnp.cov(per_instance_importance_weighted_rewards))
-            total_loss += lambda_ * jnp.sqrt(1e-10 + per_instance_importance_weighted_rewards_variance / n)
+            total_loss += lambda_ * self.variance_penalty(crm_dataset.rollout_indices,
+                                                           per_instance_importance_weighted_rewards)
 
         result = total_loss / self.k
             
         if compute_ess:
             return result, effective_sample_size
-            
+
         return result
     
     def fit(self, crm_dataset, verbose: int = 0, beta_start: float = 0,

lambda_grid.py
@@ -1,11 +1,14 @@
 import sys
-import time
-import copy
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# import jax
+# from jax.config import config as jax_config
+# jax_config.update("jax_debug_nans", True)
+
 from dataset_utils import load_dataset
-from baselines_skylines import result_table, make_baselines_skylines, stochastic_hamming_loss
+from baselines_skylines import make_baselines_skylines, stochastic_hamming_loss
 from crm_dataset import CRMDataset
 from crm_model import Model
 
@@ -24,7 +27,7 @@ samples = sorted([_ for _ in samples if _ > 100])
 n_replays = 6  # actions per context
 n_reruns = 5 # to account for action stochasticity
 
-print(samples)
+print("rollout @", samples)
 
 lambda_grid = [1e-4, 1e-3, 1e-2, 1e-1, .25, .5, .75, 1, 10, 100, 0, -1e-4, -1e-3, -1e-2, -1e-1, -.25, -.5, -.75, -1, -10, -100]
 
@@ -40,6 +43,7 @@ crm_reward = CRMDataset().update_from_supervised_dataset(
 
 
 for lambda_ in lambda_grid:
+    print('lambda:', lambda_, '*'*80)
         
     crm_losses = np.ones((n_reruns, len(samples)))
     crm_rewards = np.ones((n_reruns, len(samples)))
@@ -59,25 +63,23 @@ for lambda_ in lambda_grid:
             y = y_train[start:end, :]
             if end > start:
                 # CRM play & data collection
-                sampling_probas = np.array([_[:,1] for _ in pi0.predict_proba(X)]).T 
+                sampling_probas = np.array([_[:, 1] for _ in pi0.predict_proba(X)]).T
                 crm_dataset.update_from_supervised_dataset(X, y, sampling_probas, n_samples=n_replays)
                 # learning
-                crm_model.fit(
-                    crm_dataset, 
-                    lambda_ = lambda_,
-                    verbose = 0
-                )
+                crm_model.fit(crm_dataset, lambda_=lambda_)
             # eval
-            crm_losses[i,j] = crm_model.expected_hamming_loss(X_test, y_test)
-            crm_rewards[i,j] = crm_reward * len(X)
+            crm_losses[i, j] = crm_model.expected_hamming_loss(X_test, y_test)
+            crm_rewards[i, j] = crm_reward * len(X)
             # next round
             start = end
         print()
 
     crm_loss_by_lambda += [crm_losses.mean(axis=0)[-1]]
     crm_reward_by_lambda += [crm_rewards.mean(axis=0).sum()]
-    print("Lambda:", lambda_, "final loss:", crm_loss_by_lambda[-1], "cum reward:", crm_reward_by_lambda[-1])
-    
+    print("final loss: %.3f" % crm_loss_by_lambda[-1], "cum reward: %d" % crm_reward_by_lambda[-1])
+    print('*' * 80)
+
+
 ## S-CRM
 
 scrm_loss_by_lambda = []
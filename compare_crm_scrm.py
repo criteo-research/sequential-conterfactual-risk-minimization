547538af1a9d84d3bc9c8bd07c162dd8ae9286c7
Eustache Diemert <e.diemert@criteo.com>
2023-01-11 10:37:15 +0100

    compare_crm_scrm.py

---
+++ compare_crm_scrm.py
@@ -0,0 +1,123 @@
+import argparse
+from collections import defaultdict
+
+import numpy as np
+import pandas as pd
+
+# from jax.config import config as jax_config
+# jax_config.update("jax_debug_nans", True)
+
+from joblib import Parallel, delayed
+
+from dataset_utils import load_dataset
+from baselines_skylines import make_baselines_skylines, stochastic_hamming_loss
+from crm_dataset import CRMDataset
+from crm_model import Model
+from lambda_heuristic import rollout_indices, DEFAULT_LAMBDA_GRID
+
+
+def run_crm(X_train, y_train, X_test, y_test, pi0, samples, n_reruns, n_replays, lambda_grid,
+            scrm: bool = False,
+            autotune_lambda: bool = False, lambda_: float = .01, ips_ix: bool = False, truevar: bool = False):
+    # lambda_ is None => autotune
+
+    crm_losses = []
+    for i in range(n_reruns):
+        np.random.seed(i * 42 + 1000)
+        print('.', end='')
+
+        crm_model = Model.null_model(X_test.shape[1], y_test.shape[1])
+        crm_dataset = CRMDataset()
+
+        start = 0
+        for j, end in enumerate(samples):
+            # current batch
+            X = X_train[start:end, :]
+            y = y_train[start:end, :]
+            if end > start:
+                # CRM play & data collection
+                if not scrm or (j == 0):
+                    sampling_probas = np.array([_[:, 1] for _ in pi0.predict_proba(X)]).T
+                else:
+                    sampling_probas = crm_model.predict_proba(X, y)
+                crm_dataset.update_from_supervised_dataset(X, y, sampling_probas, n_samples=n_replays)
+                # xval lambda if needed
+                if autotune_lambda:
+                    lambda_ = Model.autotune_lambda(
+                        crm_dataset, crm_model.d, crm_model.k, grid=lambda_grid,
+                        sequential_dependence=truevar, ips_ix=ips_ix, snips=not ips_ix
+                    )
+                # learning
+                if scrm or j == len(samples) - 1:
+                    crm_model.fit(crm_dataset, lambda_=lambda_, sequential_dependence=truevar, ips_ix=ips_ix, snips=not ips_ix)
+            # next round
+            start = end
+
+        # final eval
+        crm_losses += [crm_model.expected_hamming_loss(X_test, y_test)]
+    print()
+    return crm_losses
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    def parse_datasets(x): return x.split(',')
+    parser.add_argument('datasets', type=parse_datasets)
+    parser.add_argument('--n-rollouts', '-rol', type=int, default=10)
+    parser.add_argument('--n-replays', '-rep', type=int, default=6)
+    parser.add_argument('--n-reruns', '-rer', type=int, default=10)
+    parser.add_argument('--rollout-scheme', '-rs', default='linear', choices=('linear', 'doubling'))
+    def parse_lambdas(x): return [float(_) for _ in x.split(',')]
+    parser.add_argument('--lambda-grid', '-lg', default=DEFAULT_LAMBDA_GRID, type=parse_lambdas)
+    parser.add_argument('--to', '-to', default='')
+    parser.add_argument('--n-jobs', '-j', default=1, type=int)
+    parser.add_argument('--truevar', default=False, action='store_true')
+    parser.add_argument('--ips-ix', default=False, action='store_true')
+
+    args = parser.parse_args()
+
+    results = defaultdict(list)
+
+    for dataset in args.datasets:
+        print("DATASET:", dataset)
+
+        X_train, y_train, X_test, y_test, labels = load_dataset(dataset)
+        pi0, pistar = make_baselines_skylines(dataset, X_train, y_train, n_jobs=4)
+
+        samples = rollout_indices(len(X_train), args.rollout_scheme, args.n_rollouts)
+        print("rollout @", samples)
+
+        def _run(l, scrm):
+            crm_losses = run_crm(X_train, y_train, X_test, y_test, pi0, samples,
+                                 n_reruns=args.n_reruns, n_replays=args.n_replays, lambda_grid=args.lambda_grid,
+                                 autotune_lambda=False, lambda_=l, scrm=scrm,
+                                 truevar=False, ips_ix=args.ips_ix and scrm)
+            return np.mean(crm_losses), np.std(crm_losses)
+
+        all_losses_stds_crm = Parallel(n_jobs=args.n_jobs)(delayed(_run)(l, False) for l in args.lambda_grid)
+        crm_best_loss_a_posteriori_mean, crm_best_loss_a_posteriori_std = sorted(all_losses_stds_crm)[0]
+        print(' CRM best loss a posteriori: %.5f +/- %.5f' % (
+            crm_best_loss_a_posteriori_mean, crm_best_loss_a_posteriori_std)
+        )
+
+        all_losses_stds_scrm = Parallel(n_jobs=args.n_jobs)(delayed(_run)(l, True) for l in args.lambda_grid)
+        scrm_best_loss_a_posteriori_mean, scrm_best_loss_a_posteriori_std = sorted(all_losses_stds_scrm)[0]
+        print('SCRM best loss a posteriori: %.5f +/- %.5f' % (
+            scrm_best_loss_a_posteriori_mean, scrm_best_loss_a_posteriori_std)
+        )
+
+        results['dataset'] += [dataset]
+        results['Baseline'] += ['$%.5f$' % stochastic_hamming_loss(pi0, X_test, y_test)]
+        results['CRM'] += ['$%.5f \pm %.5f$' % (crm_best_loss_a_posteriori_mean, crm_best_loss_a_posteriori_std)]
+        results['SCRM'] += ['$%.5f \pm %.5f$' % (scrm_best_loss_a_posteriori_mean, scrm_best_loss_a_posteriori_std)]
+        results['Skyline'] += ['$%.5f$' % stochastic_hamming_loss(pistar, X_test, y_test)]
+
+    df = pd.DataFrame(data=results)
+    df.to_latex(
+        'compare_crm_scrm_discrete-%s-rs_%s-ro_%d-rr_%d.tex' % (
+            args.to, args.rollout_scheme, args.n_rollouts, args.n_reruns
+        ), index=False, column_format='r', escape=False
+    )
+    print('-'*80)
+    print(df)
+    print('-'*80)

3acdd57e030b12b0f341d3c50d9c35cd0249ffc4
mat.martin <mat.martin@criteo.com>
2023-01-16 21:22:48 +0100

    script mozart

---
+++ script_mm.sh
@@ -0,0 +1,7 @@
+python compare_rl_scrm.py scene --horizon 5 &
+python compare_rl_scrm.py yeast --horizon 5 &
+python compare_rl_scrm.py tmc2007 --horizon 5 &
+python compare_rl_scrm.py scene --horizon 1 &
+python compare_rl_scrm.py yeast --horizon 1 &
+python compare_rl_scrm.py tmc2007 --horizon 1 &
+

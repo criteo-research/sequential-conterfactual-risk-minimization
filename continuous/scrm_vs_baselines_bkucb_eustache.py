6ad28287881c366ea5f216968d28a8629189a250
Houssam Zenati <houssam.zenati@inria.fr>
2023-01-16 17:47:02 +0100

    big instance experiment

--- continuous/scrm_vs_baselines_bkucb_eustache.py
+++ continuous/scrm_vs_baselines_bkucb_eustache.py
@@ -57,7 +57,7 @@ settings = {
     'lambda':0.
 }
 
-bkucb_regularization_lambda_grid = np.array([1e1, 1e-1, 1e-3])
+bkucb_regularization_lambda_grid = np.array([1e1])
 
 
 def scrm_vs_baselines(results, dataset_name, settings):
